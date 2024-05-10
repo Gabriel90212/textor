541992628fc99b10ed67c2d5b9ff95762d73c048
Gabriel K Aranda C <140840097+Gabriel90212@users.noreply.github.com>
2024-05-10 09:26:56 -0400

    Add files via upload

---
+++ word/script.js
@@ -0,0 +1,116 @@
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+
+//List of fontList
+let fontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courier New",
+    "cursive",
+];
+
+//Initial settigs
+const initializer = () => {
+    //Function 4highlighting buttons
+    //No highlighting for link, unlink, list, undo, redo since there r no time operations
+    highlighter(alignButtons, true);
+    highlighter(spacingButtons, true);
+    highlighter(formatButtons, false);
+    highlighter(scriptButtons, true);
+
+    //create options for font names
+    fontList.map((value) => {
+        let option = document.createElement("option");
+        option.value = value;
+        option.innerHTML = value;
+        fontName.appendChild(option);
+    });
+
+    //fonsize allows only till 7
+    for (let i = 1; i <= 7; i++) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.innerHTML = i;
+        fontSizeRef.appendChild(option);
+    }
+
+    //Default Size is 3
+    fontSizeRef.value = 3;
+};
+
+//main logic
+const modifyText = (command, defaultUi, value) => {
+    //execCommand executes command on selected text
+    document.execCommand(command, defaultUi, value);
+};
+
+//for basic operations which dont take value parameters
+optionsButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        modifyText(button.id, false, null);
+    });
+});
+
+//options that requires value parameters
+advancedOptionButton.forEach((button) => {
+    button.addEventListener("change", () => {
+        modifyText(button.id, false, button.value);
+    });
+});
+
+//link
+linkButton.addEventListener("click", () => {
+    let userLink = prompt("enter URL");
+    //if link has http then pass directly else add https
+    if (/http/i.test(userLink)) {
+        modifyText(linkButton.id, false, userLink);
+    } else {
+        userLink = "http://" + userLink;
+        modifyText(linkButton.id, false, userLink)
+    }
+});
+
+//Highlight clicked button
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            //NeedsRmoval = true means only one button should be highlight and other would be normal
+            if (needsRemoval) {
+                let alreadyActive = false;
+
+                //If currently clicked button is already active
+                if (button.classList.contains("active")) {
+                    alreadyActive = true;
+                }
+
+                //Remove remove highlight from other buttons
+                highlighterRemover(className); 
+                if (!alreadyActive) {
+                    //highlight clicked button
+                    button.classList.add("active");
+                }
+            } else {
+                //if other butons can be highlighted
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
+
+const highlighterRemover = (className) => {
+    className.forEach((button) => {
+        button.classList.remove("active");
+    });
+};
+window.onload = initializer();
